1fa9047544b60794a4811a182ac00648b59218f6
xbunny <pitchayarat.p@gmail.com>
2020-01-21 15:51:39 +0700

    21012020 HBD to me

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,32 @@
-//status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	
+	int age,h,ss;
+	string status;
+	cout<<"Enter your age : ";
+	cin>>age;
+	if(age<20){
+		cout<<"Enter your height : ";
+		cin>>h;
+		if(h>=160){
+			if(h>=175){
+				cout<<"Enter your property : ";
+				cin>>ss;
+				ss>69000000 ? status="MARRIED" : status="ONE-NIGHT-STAND";
+			}else{status="FRIEND";}
+		}else{status="UNFRIEND";
+			}
+	}else if(age<30){
+		cout<<"Enter your property : ";
+		cin>>ss;
+		ss>10000000 ? status="BEST FRIEND" : status ="UNFRIEND";
+	}else{
+		status = "UNFRIEND";
+	}
+	
+	cout<<"Your status = "<<status;
+	return 0;
+}
